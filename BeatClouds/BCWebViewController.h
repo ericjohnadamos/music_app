b8159d202425f4c5165c3ff72d9ecafe67316e8b
Eric John Adamos <ericjohnadamos@gmail.com>
2015-03-03 23:56:19 +0800

    BCWebViewController: Create preliminary implementation for delegate

--- BeatClouds/BCWebViewController.h
+++ BeatClouds/BCWebViewController.h
@@ -9,6 +9,16 @@
 #import <WebKit/WebKit.h>
 #import <UIKit/UIKit.h>
 
+@class BCWebViewController;
+@protocol BCWebViewDelegate <NSObject>
+
+@optional
+- (void) webViewControllerDidFinishPostLoad: (BCWebViewController*) controller;
+
+@end
+
 @interface BCWebViewController : UIViewController <WKScriptMessageHandler>
+
+@property (nonatomic, assign) id<BCWebViewDelegate> delegate;
 
 @end
